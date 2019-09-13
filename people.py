5026cbe5f95f9662f325fc695c80cc76875dcd1e
John <john.colton@infinityworks.com>
2019-09-13 11:13:17 +0100

    First commit

---
+++ people.py
@@ -0,0 +1,13 @@
+class Person:
+    def __init__(self, name, unique_id):
+        self.name = name
+        self.id = unique_id
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+    def get_id(self):
+        return self.id
---
+++ round.py
@@ -0,0 +1,23 @@
+class Round:
+    def __init__(self, brew_master):
+        self.brew_master = brew_master
+        self.orders = {}
+
+    def add_to_order(self, person, drink):
+        self.orders[person] = drink
+
+    def amend_drink(self, person, drink):
+        self.orders[person] = drink
+
+    def get_drink_orders(self):
+        return self.orders
+
+    def get_drink_count(self):
+        order_values = list(self.orders.values())
+        unique_drinks = list(set(self.orders.values()))
+        drink_value = {}
+        for drink in unique_drinks:
+            drink_value[drink] = order_values.count(drink)
+        return drink_value
+
+    # random number generator to determine who makes round
