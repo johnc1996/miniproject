dedf8958669a25f1ad7bdeab86d501dcd9fb1331
johnc1996 <john.colton@infinityworks.com>
2019-10-09 15:37:50 +0100

    Added template html files and improved flask functionality

--- source/db.py
+++ source/db.py
@@ -35,6 +35,11 @@ def get_all_drinks():
     return drinks
 
 
+def get_all_rounds():
+    query = "SELECT * FROM Rounds"
+    return db_return_rows(query)
+
+
 def get_people_first_and_last_names():
     query = "SELECT FirstName, LastName FROM People"
     first_and_last_names = db_return_rows(query)
@@ -45,3 +50,45 @@ def get_drink_names():
     query = "SELECT Name FROM Drinks"
     drink_names = db_return_rows(query)
     return drink_names
+
+
+def get_initiators_first_and_last_name():
+    query = """
+    SELECT P.FirstName, P.LastName
+    FROM Rounds as R
+    JOIN People as P ON R.InitiatorID = P.PersonID
+    """
+    return db_return_rows(query)
+
+
+def insert_drink(drink_name):
+    query = """
+    INSERT INTO Drinks(Name)
+    VALUES (%s)
+    """
+
+    parameters = drink_name.lower()
+
+    return db_insert_or_update_record(query, parameters)
+
+
+def insert_person(first_name, last_name, favourite_drink_id):
+    query = """
+    INSERT INTO People(FirstName, LastName, FavouriteDrinkID)
+    VALUES (%s, %s, %s)
+    """
+
+    parameters = (first_name.lower(), last_name.lower(), favourite_drink_id)
+
+    return db_insert_or_update_record(query, parameters)
+
+
+def insert_round(initiator_id):
+    query = """
+    INSERT INTO Rounds(InitiatorID)
+    VALUES (%s)
+    """
+
+    parameters = initiator_id
+
+    return db_insert_or_update_record(query, parameters)
--- source/flaskfunctions.py
+++ source/flaskfunctions.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify
 from flask import request
 from flask import render_template
+from flask import redirect
 
 import db
 
@@ -13,76 +14,72 @@ def get_home():
 
 
 @app.route('/people', methods=['GET'])
-def get_people_home():
-    people_first_and_last_names = db.get_people_first_and_last_names()
-    return render_template("people.html", head_title="People", page_title="People", people=people_first_and_last_names)
+def get_people():
+    if request.method == "GET":
+        people_names = db.get_people_first_and_last_names()
+        return render_template("people.html", head_title="People", page_title="People",
+                               people_names=people_names)
+
+
+@app.route('/people/add', methods=['GET', 'POST'])
+def get_add_person_page_and_add_person():
+    if request.method == 'GET':
+        favourite_drinks = db.get_all_drinks()
+        return render_template("addPerson.html", head_title="Add person", page_title="Add person",
+                               favourite_drinks=favourite_drinks)
+    elif request.method == 'POST':
+        first_name = request.form.get('first-name')
+        last_name = request.form.get('last-name')
+        favourite_drink = request.form.get('favourite-drink')
+        db.insert_person(first_name, last_name, favourite_drink)
+        return redirect('/people')
 
 
 @app.route('/drinks', methods=['GET'])
-def get_drinks_home():
+def get_drinks():
     drink_names = db.get_drink_names()
     return render_template("drinks.html", head_title="Drinks", page_title="Drinks", drink_names=drink_names)
 
 
-@app.route('/people/view', methods=['GET'])
-def get_people():
-    return render_template("viewPeople.html", head_title="View People", page_title="View People")
-
-
-@app.route('/people/add', methods=['GET', 'POST'])
-def add_person():
-    if request.method == "GET":
-        return render_template("addPerson.html", head_title="Add People", page_title="Add Person")
-    elif request.method == "POST":
-        first_name = request.form.get("first-name")
-        last_name = request.form.get("last-name")
-
-        return render_template("personAdded.html", head_title="Add People", page_title="Person Added",
-                               first_name=first_name, last_name=last_name)
-
-
-@app.route('/drinks/view', methods=['GET'])
-def get_drinks():
-    return render_template("viewDrinks.html", head_title="View Drinks", page_title="View Drinks")
-
-
 @app.route('/drinks/add', methods=['GET', 'POST'])
-def add_drink():
-    if request.method == "GET":
-        return render_template("addDrink.html", head_title="Add Drink", page_title="Add Drink")
-    elif request.method == "POST":
+def get_add_drink_page_and_add_drink():
+    if request.method == 'GET':
+        return render_template("addDrink.html", head_title="Add drink", page_title="Add drink")
+    if request.method == 'POST':
         drink_name = request.form.get("drink-name")
+        db.insert_drink(drink_name)
+        return redirect('/drinks')
 
-        return render_template("drinkAdded.html", head_title="Add Drink", page_title="Drink Added", drink=drink_name)
 
-
-@app.route('/rounds/view', methods=['GET'])
+@app.route('/rounds', methods=['GET'])
 def get_rounds():
-    return render_template("viewRounds.html", head_title="View Rounds", page_title="View Rounds")
+    rounds = db.get_initiators_first_and_last_name()
+    return render_template("rounds.html", head_title="Rounds", page_title="Rounds", rounds=rounds)
 
 
 @app.route('/rounds/add', methods=['GET', 'POST'])
-def add_round():
-    if request.method == "GET":
-        return render_template("addRound.html", head_title="Add Round", page_title="Add Round")
-    elif request.method == "POST":
-        initiator = request.form.get("round-initiator")
+def get_round_add_page_and_add_round():
+    if request.method == 'GET':
+        people = db.get_all_people()
+        return render_template("addRound.html", head_title="Add round", page_title="Add round", people=people)
+    elif request.method == 'POST':
+        initiator_id = request.form.get("initiator")
+        db.insert_round(initiator_id)
+        return redirect('/rounds')
 
-        return render_template("roundAdded.html", head_title="Add Round", page_title="Round Added",
-                               round_initiator=initiator)
 
-
-@app.route('/order/add', methods=['GET', 'POST'])
-def add_order():
-    if request.method == "GET":
-        return render_template("addOrder.html", head_title="Add Order", page_title="Add Order")
-    elif request.method == "POST":
-        person_name = request.form.get("person-name")
-        drink_name = request.form.get("drink-name")
-
-        return render_template("orderAdded.html", head_title="Add Order", page_title="Confirm Order",
-                               person=person_name, drink=drink_name)
+@app.route('/rounds/add/order', methods=['GET', 'POST'])
+def get_order_add_page_and_add_order():
+    if request.method == 'GET':
+        people = db.get_all_people()
+        drinks = db.get_all_drinks()
+        rounds = db.get_all_rounds()
+        initiators = db.get_initiators_first_and_last_name()
+        return render_template("addOrder.html", head_title="Add order", page_title="Add order", people=people,
+                               drinks=drinks, rounds=rounds, initiators=initiators)
+    elif request.method == 'POST':
+        pass
 
 
 if __name__ == "__main__":
-    app.run(host='localhost', port=15000, debug=True)
+    app.run(host='localhost', port=8000, debug=True)
